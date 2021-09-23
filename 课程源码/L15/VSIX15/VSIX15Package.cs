85600ce775bac1224d1f6812204f53eafcc8f050
zmrbak <20995658@qq.com>
2021-09-23 20:28:28 +0800

    add code

---
+++ 课程源码/L15/VSIX15/VSIX15Package.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Task = System.Threading.Tasks.Task;
+
+namespace VSIX15
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [Guid(VSIX15Package.PackageGuidString)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    public sealed class VSIX15Package : AsyncPackage
+    {
+        public const string PackageGuidString = "a3b38abb-6000-4dc6-91b3-925ff57799dd";
+
+        #region Package Members
+
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            await Command1.InitializeAsync(this);
+            await Command2.InitializeAsync(this);
+            await Command3.InitializeAsync(this);
+        }
+
+        #endregion
+    }
+}
