0e6f39cd46aff9946762828d16c5f06e0471391d
zmrbak <20995658@qq.com>
2021-10-11 20:49:11 +0800

    add code

---
+++ 课程源码/L30/VSIX30/ToolWindow1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System;
+using System.Runtime.InteropServices;
+
+namespace VSIX30
+{
+    [Guid("4c311c7c-6e33-4d09-ab3f-0d508a481c6f")]
+    public class ToolWindow1 : ToolWindowPane
+    {
+        public ToolWindow1() : base(null)
+        {
+            this.Caption = "ToolWindow1";
+
+            this.Content = new ToolWindow1Control();
+            this.ToolBar = new System.ComponentModel.Design.CommandID(
+                new Guid("3466cad1-7533-49e7-986d-7e8ad2433298"), 0x1111
+                );
+        }
+    }
+}
