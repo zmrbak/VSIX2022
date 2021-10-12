b9b88a682106a0f072d28f1f253e08918e89c416
zmrbak <20995658@qq.com>
2021-10-12 12:53:31 +0800

    add code

---
+++ 课程源码/L31/VSIX31/VSIX31Package.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Task = System.Threading.Tasks.Task;
+
+namespace VSIX31
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [Guid(VSIX31Package.PackageGuidString)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [ProvideToolWindow(typeof(ToolWindow1), MultiInstances = true)]
+    public sealed class VSIX31Package : AsyncPackage
+    {
+        public const string PackageGuidString = "97265391-4ea3-4205-b173-5cfd26d22d31";
+
+        #region Package Members
+
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            await ToolWindow1Command.InitializeAsync(this);
+        }
+
+        #endregion
+    }
+}
