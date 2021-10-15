06f8976914e08783877507be10840cc84ab57537
zmrbak <20995658@qq.com>
2021-10-15 20:19:17 +0800

    add code

---
+++ 课程源码/L32/VSIX32/ToolWindow1.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System;
+using System.Runtime.InteropServices;
+
+namespace VSIX32
+{
+    /// <summary>
+    /// This class implements the tool window exposed by this package and hosts a user control.
+    /// </summary>
+    /// <remarks>
+    /// In Visual Studio tool windows are composed of a frame (implemented by the shell) and a pane,
+    /// usually implemented by the package implementer.
+    /// <para>
+    /// This class derives from the ToolWindowPane class provided from the MPF in order to use its
+    /// implementation of the IVsUIElementPane interface.
+    /// </para>
+    /// </remarks>
+    [Guid("bf98a23b-b4b9-4956-be1d-bbe6adfea76c")]
+    public class ToolWindow1 : ToolWindowPane
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ToolWindow1"/> class.
+        /// </summary>
+        public ToolWindow1() : base(null)
+        {
+            this.Caption = "ToolWindow1";
+
+            // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
+            // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
+            // the object returned by the Content property.
+            this.Content = new ToolWindow1Control();
+        }
+    }
+}
---
+++ 课程源码/L32/VSIX32/VSIX32Package.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Task = System.Threading.Tasks.Task;
+
+namespace VSIX32
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [Guid(VSIX32Package.PackageGuidString)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [ProvideToolWindow(typeof(ToolWindow1))]
+    [ProvideToolWindowVisibility(typeof(ToolWindow1),VSConstants.UICONTEXT.SolutionExists_string)]
+    public sealed class VSIX32Package : AsyncPackage
+    {
+        public const string PackageGuidString = "21498bd8-46ec-4fa7-bc8e-426540eff8eb";
+
+        #region Package Members
+
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            await ToolWindow1Command.InitializeAsync(this);
+        }
+
+        #endregion
+    }
+}
