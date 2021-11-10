ee48f65fc1e9e2544c6ca8c289b7db65d4f00033
zmrbak <20995658@qq.com>
2021-11-10 18:07:14 +0800

    add code

---
+++ 课程源码/L35/VSIX33/SearchTask.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.Text;
+
+namespace VSIX33
+{
+    internal class SearchTask : VsSearchTask
+    {
+        public ToolWindow1 ToolWindow { get; }
+        public SearchTask(uint dwCookie, IVsSearchQuery pSearchQuery, IVsSearchCallback pSearchCallback, ToolWindow1 toolWindow)
+            : base(dwCookie, pSearchQuery, pSearchCallback)
+        {
+            ToolWindow = toolWindow;
+        }
+
+        protected override void OnStartSearch()
+        {
+            _ = OnStartSearchAsync();
+        }
+
+        protected  async System.Threading.Tasks.Task OnStartSearchAsync()
+        {
+            //ThreadHelper.ThrowIfNotOnUIThread();
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+            var separator = new string[] { Environment.NewLine };
+
+            this.ErrorCode = VSConstants.S_OK;
+            try
+            {
+                if (ToolWindow.Content is ToolWindow1Control toolWindow1Control)
+                {
+                    var contentArray = toolWindow1Control.SearchContent.Split(separator, StringSplitOptions.None);
+                    StringBuilder stringBuilder = new StringBuilder();
+                    string searchString = this.SearchQuery.SearchString;
+
+                    var matchCase = ToolWindow.MatchCaseOption.Value;
+
+                    foreach (var content in contentArray)
+                    {
+                        if(matchCase==true)
+                        {
+                            if (content.Contains(searchString))
+                            {
+                                stringBuilder.AppendLine(content);
+                            }
+                        }
+                        else
+                        {
+                            if (content.ToLower().Contains(searchString.ToLower()))
+                            {
+                                stringBuilder.AppendLine(content);
+                            }
+                        }                       
+                    }
+                    toolWindow1Control.SearchResultsTextBox.Text = stringBuilder.ToString();
+                }
+            }
+            catch 
+            {
+                this.ErrorCode = VSConstants.E_FAIL;
+            }
+
+
+            base.OnStartSearch();
+        }
+
+        protected override void OnStopSearch()
+        {
+            base.OnStopSearch();
+        }
+
+    }
+}
---
+++ 课程源码/L35/VSIX33/ToolWindow1.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.PlatformUI;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace VSIX33
+{
+    [Guid("f376b4c5-139c-4b5f-b543-bea7ed05c7a3")]
+    public class ToolWindow1 : ToolWindowPane
+    {
+        private IVsEnumWindowSearchOptions searchOptionsEnum;
+        private WindowSearchBooleanOption matchCaseOption;
+
+        public ToolWindow1() : base(null)
+        {
+            this.Caption = "ToolWindow1";
+
+            this.Content = new ToolWindow1Control();
+        }
+
+        public override IVsEnumWindowSearchOptions SearchOptionsEnum
+        {
+            get
+            {
+                if(searchOptionsEnum==null)
+                {
+                    var list = new List<IVsWindowSearchOption>();
+                    list.Add(MatchCaseOption);
+                    //list.Add(new WindowSearchBooleanOption("匹配大小写", "匹配大小写",false));
+                    searchOptionsEnum = new WindowSearchOptionEnumerator(list);
+                }
+                return searchOptionsEnum;
+            }
+        }
+
+        public override bool SearchEnabled => true;
+
+        public WindowSearchBooleanOption MatchCaseOption
+        {
+            get
+            {
+                if(matchCaseOption==null)
+                {
+                    matchCaseOption=new WindowSearchBooleanOption("匹配大小写", "匹配大小写", false);
+                }
+                return matchCaseOption;
+            }
+        }
+
+        public override IVsSearchTask CreateSearch(uint dwCookie, IVsSearchQuery pSearchQuery, IVsSearchCallback pSearchCallback)
+        {
+            if (pSearchQuery == null || pSearchCallback == null) return null;
+
+            return new SearchTask(dwCookie, pSearchQuery, pSearchCallback, this);
+        }
+        public override void ClearSearch()
+        {
+            if(this.Content is ToolWindow1Control toolWindow1Control)
+            {
+                toolWindow1Control.SearchResultsTextBox.Text = toolWindow1Control.SearchContent;
+            }
+        }
+    }
+}
