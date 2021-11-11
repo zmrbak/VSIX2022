72caf54784468bb1c6485b021256f019c54f23c2
zmrbak <20995658@qq.com>
2021-11-11 18:48:24 +0800

    add code

---
+++ 课程源码/L37/VSIX37/DeleteOutPutWindow1.cs
@@ -0,0 +1,127 @@
+﻿using EnvDTE;
+using Microsoft;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using Task = System.Threading.Tasks.Task;
+
+namespace VSIX37
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class DeleteOutPutWindow1
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 4129;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("d76b171b-f1df-4dbc-9956-e860636d1ae2");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DeleteOutPutWindow1"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private DeleteOutPutWindow1(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+       
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static DeleteOutPutWindow1 Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in DeleteOutPutWindow1's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new DeleteOutPutWindow1(package, commandService);
+        }
+
+        Guid guid = Guid.NewGuid();
+        bool isCreated=false;
+
+        private void Execute(object sender, EventArgs e)
+        {
+            _ = ExecuteAsync();
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private async Task ExecuteAsync()
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+            IVsOutputWindow outputWindow=await ServiceProvider.GetServiceAsync(typeof(SVsOutputWindow)) as IVsOutputWindow;
+            Assumes.Present(outputWindow);
+
+            IVsOutputWindowPane outputWindowPane;
+
+            string paneTitle = VSIX37Package.PaneTitle;
+
+            if(isCreated==false)
+            {
+                outputWindow.CreatePane(ref guid, paneTitle, Convert.ToInt32(true), Convert.ToInt32(false));
+                isCreated=true;
+            }
+            else
+            {
+                outputWindow.GetPane(ref guid, out outputWindowPane);
+                if(outputWindowPane!=null)
+                {
+                    outputWindow.DeletePane(guid);
+                }
+                isCreated = false;
+            }           
+        }
+    }
+}
---
+++ 课程源码/L37/VSIX37/VSIX37Package.cs
@@ -0,0 +1,61 @@
+﻿using EnvDTE;
+using Microsoft.VisualStudio.Shell;
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Task = System.Threading.Tasks.Task;
+
+namespace VSIX37
+{
+    /// <summary>
+    /// This is the class that implements the package exposed by this assembly.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// The minimum requirement for a class to be considered a valid package for Visual Studio
+    /// is to implement the IVsPackage interface and register itself with the shell.
+    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
+    /// to do it: it derives from the Package class that provides the implementation of the
+    /// IVsPackage interface and uses the registration attributes defined in the framework to
+    /// register itself and its components with the shell. These attributes tell the pkgdef creation
+    /// utility what data to put into .pkgdef file.
+    /// </para>
+    /// <para>
+    /// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
+    /// </para>
+    /// </remarks>
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [Guid(VSIX37Package.PackageGuidString)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    public sealed class VSIX37Package : AsyncPackage
+    {
+        /// <summary>
+        /// VSIX37Package GUID string.
+        /// </summary>
+        public const string PackageGuidString = "58c8934f-c4cc-4832-8c80-429e12c4b9a7";
+        public const string PaneTitle = "My Test Pane";
+
+        #region Package Members
+
+        /// <summary>
+        /// Initialization of the package; this method is called right after the package is sited, so this is the place
+        /// where you can put all the initialization code that rely on services provided by VisualStudio.
+        /// </summary>
+        /// <param name="cancellationToken">A cancellation token to monitor for initialization cancellation, which can occur when VS is shutting down.</param>
+        /// <param name="progress">A provider for progress updates.</param>
+        /// <returns>A task representing the async work of package initialization, or an already completed task if there is none. Do not return null from this method.</returns>
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            // When initialized asynchronously, the current thread may be a background thread at this point.
+            // Do any initialization that requires the UI thread after switching to the UI thread.
+            await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            await OutPutWindow1.InitializeAsync(this);
+            await DeleteOutPutWindow1.InitializeAsync(this);
+            await WirteOutPutWindow1.InitializeAsync(this);
+
+            //await this.GetServiceAsync(typeof(DTE));
+        }
+
+        #endregion
+    }
+}
