83dbfd280795c3401a4a71f8c880cc7cf92cf5fd
zmrbak <20995658@qq.com>
2021-11-15 17:16:48 +0800

    add code

---
+++ 课程源码/L45/VSIX45/TextAdornment1.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Formatting;
+using System;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace VSIX45
+{
+    /// <summary>
+    /// TextAdornment1 places red boxes behind all the "a"s in the editor window
+    /// TextAdorment1将红色框放在编辑器窗口中所有“a”的后面
+    /// </summary>
+    internal sealed class TextAdornment1
+    {
+        /// <summary>
+        /// The layer of the adornment.
+        /// 装饰的层
+        /// </summary>
+        private readonly IAdornmentLayer layer;
+
+        /// <summary>
+        /// Text view where the adornment is created.
+        /// 创建装饰的文本视图
+        /// </summary>
+        private readonly IWpfTextView view;
+
+        /// <summary>
+        /// Adornment brush.
+        /// 装饰画刷
+        /// </summary>
+        private readonly Brush brush;
+
+        /// <summary>
+        /// Adornment pen.
+        /// 装饰画笔
+        /// </summary>
+        private readonly Pen pen;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TextAdornment1"/> class.
+        /// 初始化TextAdornment1类的新实例
+        /// </summary>
+        /// <param name="view">
+        /// Text view to create the adornment for 
+        /// 要为其创建装饰的文本视图
+        /// </param>
+        public TextAdornment1(IWpfTextView view)
+        {
+            if (view == null)
+            {
+                throw new ArgumentNullException("view");
+            }
+
+            this.layer = view.GetAdornmentLayer("TextAdornment1");
+
+            this.view = view;
+            this.view.LayoutChanged += this.OnLayoutChanged;
+
+            // Create the pen and brush to color the box behind the a's
+            // 创建画刷和画笔，为a后面的方框上色
+            this.brush = new SolidColorBrush(Color.FromArgb(0x20, 0x00, 0x00, 0xff));
+            this.brush.Freeze();
+
+            var penBrush = new SolidColorBrush(Colors.Red);
+            penBrush.Freeze();
+            this.pen = new Pen(penBrush, 0.5);
+            this.pen.Freeze();
+        }
+
+        /// <summary>
+        /// Handles whenever the text displayed in the view changes by adding the adornment to any reformatted lines
+        /// 当装饰添加到每一个重新格式化的行中，视图中显示的文本发生更改时，都会进行处理
+        /// </summary>
+        /// <remarks>
+        /// <para>This event is raised whenever the rendered text displayed in the <see cref="ITextView"/> changes.
+        /// 当ITextView中显示的渲染文本发生更改时，将引发此事件
+        /// </para>
+        /// <para>It is raised whenever the view does a layout (which happens when DisplayTextLineContainingBufferPosition is called or in response to text or classification changes).
+        /// 当视图进行布局时引发该事件。（在调用DisplayTextLineContainingBufferPosition或响应文本或分类更改时）
+        /// </para>
+        /// <para>It is also raised whenever the view scrolls horizontally or when its size changes.
+        /// 当视图水平滚动或其大小更改时，也会引发该事件
+        /// </para>
+        /// </remarks>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event arguments.</param>
+        internal void OnLayoutChanged(object sender, TextViewLayoutChangedEventArgs e)
+        {
+            foreach (ITextViewLine line in e.NewOrReformattedLines)
+            {
+                this.CreateVisuals(line);
+            }
+        }
+
+        /// <summary>
+        /// Adds the scarlet box behind the 'a' characters within the given line
+        /// 在给定行中的“a”字符背后添加猩红色框
+        /// </summary>
+        /// <param name="line">
+        /// Line to add the adornments 
+        /// 将被添加装饰的行
+        /// </param>
+        private void CreateVisuals(ITextViewLine line)
+        {
+            IWpfTextViewLineCollection textViewLines = this.view.TextViewLines;
+
+            // Loop through each character, and place a box around any 'a'
+            // 循环遍历每个字符，并在任何“a”周围放置一个框
+            for (int charIndex = line.Start; charIndex < line.End; charIndex++)
+            {
+                if (this.view.TextSnapshot[charIndex] == 'a')
+                {
+                    SnapshotSpan span = new SnapshotSpan(this.view.TextSnapshot, Span.FromBounds(charIndex, charIndex + 1));
+                    Geometry geometry = textViewLines.GetMarkerGeometry(span);
+                    if (geometry != null)
+                    {
+                        var drawing = new GeometryDrawing(this.brush, this.pen, geometry);
+                        drawing.Freeze();
+
+                        var drawingImage = new DrawingImage(drawing);
+                        drawingImage.Freeze();
+
+                        var image = new Image
+                        {
+                            Source = drawingImage,
+                        };
+
+                        // Align the image with the top of the bounds of the text geometry
+                        // 将图像与文本几何体边界的顶部对齐
+                        Canvas.SetLeft(image, geometry.Bounds.Left);
+                        Canvas.SetTop(image, geometry.Bounds.Top);
+
+                        this.layer.AddAdornment(AdornmentPositioningBehavior.TextRelative, span, null, image, null);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 课程源码/L45/VSIX45/TextAdornment1TextViewCreationListener.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+using System.ComponentModel.Composition;
+
+namespace VSIX45
+{
+    /// <summary>
+    /// Establishes an <see cref="IAdornmentLayer"/> to place the adornment on and exports the <see cref="IWpfTextViewCreationListener"/>
+    /// that instantiates the adornment on the event of a <see cref="IWpfTextView"/>'s creation
+    /// 
+    /// 建立一个IAdornmentLayer来放置装饰，并在IWpfTextView的创建事件初始化装饰时，导出IWpfTextViewCreationListener。
+    /// </summary>
+    [Export(typeof(IWpfTextViewCreationListener))]
+    [ContentType("text")]
+    [TextViewRole(PredefinedTextViewRoles.Document)]
+    internal sealed class TextAdornment1TextViewCreationListener : IWpfTextViewCreationListener
+    {
+        // Disable "Field is never assigned to..." and "Field is never used" compiler's warnings. Justification: the field is used by MEF.
+        // 禁用“字段从未分配给…”和“字段从未使用”编译器的警告。原因：该字段由MEF使用。
+#pragma warning disable 649, 169
+
+        /// <summary>
+        /// Defines the adornment layer for the adornment. This layer is ordered
+        /// after the selection layer in the Z-order
+        /// 定义装饰的装饰层。该层按Z顺序排列在选择层之后
+        /// </summary>
+        [Export(typeof(AdornmentLayerDefinition))]
+        [Name("TextAdornment1")]
+        [Order(After = PredefinedAdornmentLayers.Selection, Before = PredefinedAdornmentLayers.Text)]
+        private AdornmentLayerDefinition editorAdornmentLayer;
+
+#pragma warning restore 649, 169
+
+        #region IWpfTextViewCreationListener
+
+        /// <summary>
+        /// Called when a text view having matching roles is created over a text data model having a matching content type.
+        /// 在具有匹配内容类型的文本数据模型上创建具有匹配角色的文本视图时调用。
+        /// Instantiates a TextAdornment1 manager when the textView is created.
+        /// 创建textView时，实例化TextAdorment1管理器。
+        /// </summary>
+        /// <param name="textView">The <see cref="IWpfTextView"/> upon which the adornment should be placed
+        /// 装饰应被放置到上面的IWpfTextView
+        /// </param>
+        public void TextViewCreated(IWpfTextView textView)
+        {
+            // The adornment will listen to any event that changes the layout (text changes, scrolling, etc)
+            new TextAdornment1(textView);
+        }
+
+        #endregion
+    }
+}
