74949fec6d1c5bc45fa71dcf76d3acddd9a9b96c
zmrbak <20995658@qq.com>
2021-11-19 17:13:43 +0800

    add code

---
+++ 课程源码/L48/VSIX48/Class1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSIX48
+{
+    internal /*static*/ class Class1
+    {
+#pragma warning disable 649
+        [Export]
+        [Name("zmrbak")]        
+        [BaseDefinition("text")]
+        internal /*static*/ ContentTypeDefinition exampleDefinition;
+
+
+        [Export]
+        [FileExtension(".zmrbak")]  
+        [ContentType("zmrbak")]
+        internal /*static*/ FileExtensionToContentTypeDefinition abcFileExtensionDefinition;
+#pragma warning restore 649
+    }
+}
